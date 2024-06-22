de18e2ebae5ef682a985ec5918cdc71300652786
Andrew Adjei Laryea <Andrew.Laryea@cdhlife.com>
2024-06-22 09:38:54 +0000

    Performed arithmetic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+# Define Variables 
+number1 = 10
+number2 = 5
+
+# Performig arithmetic operations
+addition_result = number1 + number2
+subtraction_result = number1 - number2
+multiplication_result = number1 * number2
+
+#Display outputs/results
+print(f"Addition of {number1} and {number2} is {addition_result}")
+print(f"Subtraction of {number1} and {number2} is {subtraction_result}")
+print(f"Multiplication of {number1} and {number2} is {multiplication_result}")

4bf10dfcdcb5d02df3a02097d17d0202105009c9
Andrew Adjei Laryea <Andrew.Laryea@cdhlife.com>
2024-06-23 15:52:59 +0000

    updated finance file

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -3,12 +3,12 @@ monthly_income = float(input("Enter your monthly income: "))
 monthly_expense = float(input("Enter your total monthly expenses: "))
 
 #Calculate monthly savings 
-monthly_savings = float(monthly_income - monthly_expense)
+monthly_savings = monthly_income - monthly_expense
 
 # project annual savings with a fixed interest rate of 5%
 annual_interest_rate = 0.05
 Projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
 
 #print the results
-print(f"Your monthly savings are ${monthly_savings}.")
-print(f"Projected savings after one year, with interest, is: ${Projected_savings}.")
+print(f"Your monthly savings are ${int(monthly_savings)}.")
+print(f"Projected savings after one year, with interest, is: ${int(Projected_savings)}.")

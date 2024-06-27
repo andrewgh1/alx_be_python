3c827ea4d669f7574f6cde950d302246c52fea5a
Andrew Adjei Laryea <Andrew.Laryea@cdhlife.com>
2024-06-27 12:24:39 +0000

    created a match_case calculator

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,32 @@
+# match_case_calculator.py
+
+# Prompt user for input
+num1 =float(input("Enter the first number: "))
+num2 =float(input("Enter the second number: "))
+
+# Prompt user for operation choice
+operation = input("Choose the operation (-, *, +, /): ")
+
+
+# Perform calculation using match-case statement
+match operation:
+    case "+":
+        result = num1 + num2
+    case "-":
+        result = num1 - num2
+    case "*":
+        result = num1 * num2
+    case "/":
+        if num2 == 0:
+            print("Cannot divide by zero.")
+        else:
+            result = num1 / num2
+    case _:
+        print("Invalid operation selected.")
+
+    
+# Output the result
+if operation == "/" and num2 != 0 :
+    print(f"The result is {result:.2f}.")
+elif operation in ["+", "-", "*"]:
+    print(f"The result is {result}.")

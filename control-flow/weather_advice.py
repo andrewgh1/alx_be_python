da6d59aa389628450e4e7665928a16bfc79cffec
Andrew Adjei Laryea <Andrew.Laryea@cdhlife.com>
2024-06-27 12:10:51 +0000

    created a contol flow and weather py file

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,14 @@
+# weather_advice.py
+
+# Prompt user for weather input
+weather = input("What's the weather like today? (sunny/rainy/cold): ")
+
+# Provide clothing recommendations based on weather input
+if weather == "sunny":
+    print("Wear a t-shirt and sunglasses.")
+elif weather == "rainy":
+    print("Don't forget your umbrella and a raincoat.")
+elif weather == "cold":
+    print("Make sure to wear a warm coat and a scarf.")
+else:
+    print("Sorry, I don't have recommendations for this weather.")

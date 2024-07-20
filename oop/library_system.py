8ddb726996266850db1ad3a37f4b8d0ae93e5773
Andrew Adjei Laryea <Andrew.Laryea@cdhlife.com>
2024-07-20 01:53:45 +0000

    Update on library system OOP

--- oop/library_system.py
+++ oop/library_system.py
@@ -4,7 +4,7 @@ class Book:
         self.author = author
 
     def __str__(self):
-        return f"'{self.title}' by {self.author}"
+        return f"Book: '{self.title}' by {self.author}"
 
 class EBook(Book):
     def __init__(self, title, author, file_size):
@@ -12,7 +12,7 @@ class EBook(Book):
         self.file_size = file_size
 
     def __str__(self):
-        return f"'{self.title}' by {self.author} (EBook, {self.file_size}MB)"
+        return f"EBook: '{self.title}' by {self.author} (EBook, {self.file_size}MB)"
 
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
@@ -20,7 +20,7 @@ class PrintBook(Book):
         self.page_count = page_count
 
     def __str__(self):
-        return f"'{self.title}' by {self.author} (PrintBook, {self.page_count} pages)"
+        return f"PrintBook: '{self.title}' by {self.author} (PrintBook, {self.page_count} pages)"
 
 class Library:
     def __init__(self):
